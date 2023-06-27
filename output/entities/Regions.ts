4a35cf45a4a53521064b6972f09ae0dec1b9886c
naufalfirdaus <naufalfirdausa@gmail.com>
2023-06-27 12:43:45 +0700

    day02

---
+++ output/entities/Regions.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  Entity,
+  Index,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Countries } from "./Countries";
+
+@Index("regions_pkey", ["regionId"], { unique: true })
+@Entity("regions", { schema: "public" })
+export class Regions {
+  @PrimaryGeneratedColumn({ type: "integer", name: "region_id" })
+  regionId: number;
+
+  @Column("character varying", {
+    name: "region_name",
+    nullable: true,
+    length: 22,
+  })
+  regionName: string | null;
+
+  @Column("character varying", { name: "photo", nullable: true, length: 50 })
+  photo: string | null;
+
+  @OneToMany(() => Countries, (countries) => countries.region)
+  countries: Countries[];
+}
